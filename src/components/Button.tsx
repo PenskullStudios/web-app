935657010cbaf62152aac7a2585ffecd470dcfd4
Codensity <codensity30@gmail.com>
2023-12-13 15:36:48 +0530

    bug fixes

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Button({ info, link }: { info: string; link: string }) {
-  return (
-    <Link href={link}>
-      <button className="btn btn-neutral">{info}</button>
-    </Link>
-  );
-}
